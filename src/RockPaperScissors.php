a31c9322d966d57836ad74fcb114baa85fc95ff7
nathan young & julian stewart <student@epicodus.com>
2015-08-12 14:09:24 -0700

    pass all tests

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -4,7 +4,7 @@
         function playRockPaperScissors($input_one, $input_two)
         {
             $game_result = "";
-            if ($input_one == $input_two) {
+            if (($input_one == $input_two) && (($input_one == "rock" || ($input_one == "paper") || $input_one == "scissors")) && (($input_two == "rock") || ($input_two == "paper") || ($input_two == "scissors"))) {
                 $game_result = "Draw";
             } elseif (($input_one == "rock") && ($input_two == "paper")) {
                 $game_result = "p2";
@@ -18,6 +18,8 @@
                 $game_result = "p2";
             } elseif (($input_one == "scissors") && ($input_two == "paper")) {
                 $game_result = "p1";
+            } elseif (($input_one == (string)"") && ($input_two == (string)"")) {
+                $game_result = "error";
             } else $game_result = "error";
             return $game_result;
 
