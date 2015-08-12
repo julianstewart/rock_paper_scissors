34c270771c50e4da4737d0c30d0afcb12b939346
nathan young & julian stewart <student@epicodus.com>
2015-08-12 11:57:15 -0700

    pass paper scissors test

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -12,6 +12,8 @@
                 $game_result = "p1";
             } elseif (($input_one == "paper") && ($input_two == "rock")) {
                 $game_result = "p1";
+            } elseif (($input_one == "paper") && ($input_two == "scissors")) {
+                $game_result = "p2";
             }
             return $game_result;
 
